f113f717b71ddc5581240ccc8ad802aaced3fedc
Shikshya Poudel <Cixya123@gmail.com>
2025-02-12 07:30:21 +0545

    Variable, DataTypes and Operators

---
+++ 2day.py
@@ -0,0 +1,67 @@
+#variable
+
+# x = 5
+# y = 10
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+
+
+#naming convention
+
+# age = 24
+# score = 100
+# isLoggedIn = 2       # camelcasing
+# is_logged_in = 3     # snake casing
+# IsLoggedIn = 4       #pascal casing
+# bg-color = red         #kebab casing
+# MAX_LIMIT = 20         #upper snake case
+# PI = 3.14
+
+
+# a = 10
+# b = 20
+# c = 30
+# print(a)
+# print(b)
+# del a
+# print(b)
+# print(c)
+
+
+#datatype
+
+# x = "Hello World"  #string
+# x = 20             #int
+# x = 20.5           #float
+# x = 1j             #complex
+# x = ["apple", "banana", "grapes"]    #list
+# x = ("apple", "banana", "grapes")    #tuple
+# x = range(6)        #range
+# z = {"name": "John", "age": 36}      #dict
+# x = {"apple", "banana", "cherry"}    #set
+# x = frozenset({"apple", "banana", "cherry"})  #frozenset
+# x = True            #bool
+# x = None            #NoneType
+
+
+# age = 20
+# temperature = 32.21
+# print(type(age))
+# print(type(temperature))
+
+
+#Operators
+# num1 = 10
+# num2 = 20
+# print(num1 == num2)
+# print(num1 != num2)
+# print(num1 > num2)
+# print(num1 < num2)
+# print(num1 >= num2)
+# print(num1 <= num2)
+
+
+
